b4bdf8f5b5ed9f6e4a1a5dbd410a9d91b4fce07e
Yazdun <yazduntube@gmail.com>
2023-03-04 18:59:10 +0330

    Added unmount animation to the modal

--- components/Generated.tsx
+++ components/Generated.tsx
@@ -18,29 +18,19 @@ export default function Generated({
 }: Props) {
   return (
     <div className="fixed top-0 left-0 z-40 flex items-center justify-center w-full h-screen isolate">
-      <div
+      <motion.div
+        {...framer_background}
         onClick={() => setGenerated("")}
-        className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-80 -z-10"
-      ></div>
-      <div className="bg-white w-full max-w-[800px] shadow-2xl p-4 rounded-lg overflow-hidden border-2 border-gray-200">
+        className="absolute top-0 left-0 w-full h-full backdrop-blur-sm bg-[rgba(255,255,255,0.4)] -z-10"
+      ></motion.div>
+      <motion.div
+        {...framer_modal}
+        className="bg-white w-full max-w-[800px] shadow-2xl p-4 rounded-lg overflow-hidden border-2 border-gray-200"
+      >
         <h2 className="my-4 mt-6 text-2xl font-medium">Your Sentence</h2>
-        <motion.p
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.4 }}
-          className="p-4 rounded-md bg-slate-200"
-        >
-          {promptValue}
-        </motion.p>
+        <p className="p-4 rounded-md bg-slate-200">{promptValue}</p>
         <h2 className="my-4 mt-6 text-2xl font-medium">Generated Sentence</h2>
-        <motion.p
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.4, delay: 0.1 }}
-          className="p-4 text-white bg-black rounded-md"
-        >
-          {generated}
-        </motion.p>
+        <p className="p-4 text-white bg-black rounded-md">{generated}</p>
         <button
           onClick={() => copingText(generated)}
           className="p-4 mt-8 text-white duration-100 bg-black rounded-md focus:bg-green-700"
@@ -55,7 +45,21 @@ export default function Generated({
             position: "bottom-right",
           }}
         />
-      </div>
+      </motion.div>
     </div>
   );
 }
+
+export const framer_background = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0, transition: { delay: 0.2 } },
+  transition: { duration: 0.3 },
+};
+
+export const framer_modal = {
+  initial: { y: "5%", opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  exit: { y: "5%", opacity: 0 },
+  transition: { duration: 0.3 },
+};
